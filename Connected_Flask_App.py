4e71616f96dcf7e05f7fef1906f4305df5648946
ViliLuosujarvi <viluosuj@edu.lapinamk.fi>
2026-05-20 19:29:17 +0200

    Flask update

--- Connected_Flask_App.py
+++ Connected_Flask_App.py
@@ -1,3 +1,9 @@
+# ----------------------------------------------------------------
+# This file is still in progress and it's purpose is to serve
+# as mock connection between the database and the web application 
+# ----------------------------------------------------------------
+
+
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
